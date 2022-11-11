a49eddb1f955433117b39ed58d05b1eb81bf309a
Denis070987 <106919801+Denis070987@users.noreply.github.com>
2022-11-11 09:38:58 +0000

    Deploying to gh-pages from @ Denis070987/goit-js-hw-09@aaba0f49876ed5f9d25490bba992797c9ce65e86 🚀

---
+++ 01-color-switcher.d283e455.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((()=>{document.body.style.backgroundColor=[`#${Math.floor(16777215*Math.random()).toString(16)}`]}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.d283e455.js.map
