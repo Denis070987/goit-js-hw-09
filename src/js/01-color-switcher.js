8870772bec48939d28baf0a45399763f95796cee
Denis070987 <deniskoms@gmail.com>
2022-11-11 17:34:42 +0200

    fix task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,10 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
 startBtn.addEventListener('click', (changeColor))
 function changeColor() {
-    timerId = setInterval(() => { document.body.style.backgroundColor = [getRandomHexColor()] }, 1000)
+  timerId = setInterval(() => { document.body.style.backgroundColor = [getRandomHexColor()] }, 1000)
        startBtn.setAttribute('disabled', true)
    }
 
@@ -11,7 +12,6 @@ stopBtn.addEventListener('click', () => {
     clearInterval(timerId);
     startBtn.removeAttribute('disabled')
 })
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
